d5bb1b2e79e4bee2b687f0bba50b6f0ba6283cb5
Vitor Lamartine <vitorlamartine21@gmail.com>
2023-02-23 19:52:00 -0300

    Correção comando ping

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(interaction) {
-        await interaction.reply('Pong! Local');
+        await interaction.reply('Pong!');
     },
 };
